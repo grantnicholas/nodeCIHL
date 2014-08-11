f34e2a0a70159a8dde5fb21ec474541c28534b75
Grant <grantnicholas2015@u.northwestern.edu>
2014-08-10 21:44:15 -0500

    create game functioning

---
+++ routes/placeholders/login.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/login', function(req,res){
+	console.log(req.body.username);
+	console.log(req.body.password);
+	res.render('showresults', {username : req.body.username, password : req.body.password});
+});
+
+module.exports = router;
